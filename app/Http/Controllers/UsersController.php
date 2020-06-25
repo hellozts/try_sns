6d84e1ef546389465c2291cc866c67e9cf6a8db7
hellozts <hellozts@gmail.com>
2020-06-25 12:58:15 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,19 @@ class UsersController extends Controller
     public function show(User $user) {
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $req) {
+        $this->validate($req, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => bcrypt($req->password)
+        ]);
+        session()->flash('success', '欢迎， 您将在这里开启一段新的旅程');
+        return redirect()->route('users.show', [$user]);
+    }
 }
