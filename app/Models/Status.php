3fbffe4363a09e17f22aaacb6a7cfb6721f0fa12
hellozts <hellozts@gmail.com>
2020-06-25 23:00:53 +0800

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -11,4 +11,8 @@ class Status extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
 }
