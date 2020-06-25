af2a5f70d0978b105d21dd64ce3f64a6291d630d
hellozts <hellozts@gmail.com>
2020-06-26 00:50:59 +0800

    动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,9 @@ class User extends Authenticatable
     }
 
     public function feed() {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     public function followers() {
@@ -83,7 +85,7 @@ class User extends Authenticatable
         if(!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
-        $this->followings()->sync($user_ids);
+        $this->followings()->sync($user_ids, false);
     }
 
     public function unfollow($user_ids) {
--- app/Policies/StatusPolicy.php
+++ app/Policies/StatusPolicy.php
@@ -11,6 +11,6 @@ class StatusPolicy
     use HandlesAuthorization;
 
     public function destroy(User $user, Status $status) {
-        return $status->user_id = $user->id;
+        return $status->user_id === $user->id;
     }
 }
