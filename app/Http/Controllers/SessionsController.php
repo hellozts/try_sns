3058a131e824da2ec7a9dea7bf248beab464ba8c
hellozts <hellozts@gmail.com>
2020-06-25 20:10:30 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class SessionsController extends Controller
 {
@@ -25,9 +26,15 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
         if(Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来!');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
+            if(Auth::user()->activated) {
+                session()->flash('success', '欢迎回来!');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号还未激活，请检查邮箱中de注册邮件进行激活.');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', '很抱歉，您的邮箱或者密码不匹配');
             return redirect()->back()->withInput();
@@ -38,5 +45,17 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success', '您已经成功退出登录');
         return redirect('login');
+    }
+
+    protected function sendEmailConfirmationTo($user) {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'hellozts@gmail.com';
+        $name = 'hellozts';
+        $to = $user->email;
+        $subject = '感谢注册微博应用! 请确认你的邮箱';
+        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 }
